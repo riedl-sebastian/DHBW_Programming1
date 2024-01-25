b17af01316406be86da54ccc7ea2c5981ed69329
riedl-sebastian <sebastian.riedl0405@gmail.com>
2024-01-25 01:03:00 +0100

    Non-Abundant Sums

--- Problem 38.py
+++ Problem 38.py
@@ -13,3 +13,20 @@ for i in range(1, 10000):
             max_val = max(val, max_val)
 
 print(max_val)
+
+# Noch ein Problem
+def abundant(n):
+    factors = set()
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            factors.add(i)
+            factors.add(n//i)
+    return sum(factors) - n > n
+
+abundant_numbers = [i for i in range(1, 28124) if abundant(i)]
+abundant_sums = {i+j for i in abundant_numbers for j in abundant_numbers if i+j < 28124}
+
+all_numbers = set(range(1, 28124))
+non_abundant_sums = all_numbers - abundant_sums
+
+print(sum(non_abundant_sums))

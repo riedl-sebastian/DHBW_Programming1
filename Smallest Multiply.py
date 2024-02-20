d57956440f78bb3723aff544234be340c931c26c
riedl-sebastian <sebastian.riedl0405@gmail.com>
2024-02-20 08:16:52 +0100

    Erweiterung Dreiecks-, Fünfecks- und Sechseckszahlen

--- Smallest Multiply.py
+++ Smallest Multiply.py
@@ -11,3 +11,23 @@ def kgT(n):
     return x
 
 print(kgT(20))
+
+# Project Euler Dreieckszahl:
+
+def dreieck(n):
+    return n * (n + 1) // 2
+
+def fünfeck(n):
+    return n * (3*n - 1) // 2
+
+def sechseck(n):
+    return n * (2*n - 1)
+
+def euler_45():
+    T = {dreieck(n) for n in range(286, 100000)}
+    P = {fünfeck(n) for n in range(166, 100000)}
+    H = {sechseck(n) for n in range(144, 100000)}
+
+    return min(T & P & H)
+
+print(euler_45())

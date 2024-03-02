79a9927983d5aff41a28253a5663d00f2db825d1
riedl-sebastian <sebastian.riedl0405@gmail.com>
2024-03-02 10:11:34 +0100

    Länge Array a^b ProjectEuler

---
+++ a^b.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+ergebnisse = []
+
+for a in range(2, 6):
+    for b in range(2, 6):
+        ergebnis = a ** b
+        ergebnisse.append(ergebnis)
+
+ergebnisse = np.array(ergebnisse)
+
+print("Die Länge des Arrays ist:", len(ergebnisse))
